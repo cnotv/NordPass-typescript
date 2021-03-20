0968f5a4ec9ca9c0727a42e1fff60e6998f8db48
Giuseppe Leo <giuseppe.leo@retresco.de>
2021-03-20 22:49:32 +0100

    Correct passowrd week detection logic (test fix)

--- src/utils/passwordValidation.ts
+++ src/utils/passwordValidation.ts
@@ -1,6 +1,6 @@
 export const isWeakPassword = (password: string): boolean => [
   password.match(/[a-z]/) != null,
   password.match(/[A-Z]/) != null,
-  password.match(/[!@#$%^&*]/) != null,
+  password.match(/[!@#$%^&*~]/) != null,
   password.match(/[0-9]/) != null,
-].filter(Boolean).length  > 2;
+].includes(false);
